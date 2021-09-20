f8926e201122c52172189438a422d65139e4cf49
PawelDabala <pdabala@gmail.com>
2021-09-20 14:01:04 +0200

    use make_dataclass

--- 001_DataClass/main.py
+++ 001_DataClass/main.py
@@ -7,6 +7,19 @@ class DataClassCard:
 
 
 queen_of_hearts = DataClassCard('Q', 'Hearts')
-print(queen_of_hearts.rank)
-print(queen_of_hearts)
-print(queen_of_hearts == DataClassCard('Q', 'Hearts'))
+# print(queen_of_hearts.rank)
+# print(queen_of_hearts)
+# print(queen_of_hearts == DataClassCard('Q', 'Hearts'))
+
+
+#Basic Data Casses
+@dataclass
+class Position:
+    name: str
+    lon: float
+    lat: float
+
+pos = Position('Oslo', 10.8, 59.9)
+print(pos)
+print(pos.lat)
+print(pos.name)
