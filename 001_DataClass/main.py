2aa706815d58f6718bf6ce97cde7377fa4773d98
PawelDabala <pdabala@gmail.com>
2021-09-22 10:24:29 +0200

    set list in dataclass

--- 001_DataClass/main.py
+++ 001_DataClass/main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, make_dataclass
+from typing import List
 
 @dataclass
 class DataClassCard:
@@ -37,3 +38,44 @@ class Position:
 
 print(Position('Null Island'))
 print(Position('Greenwich', lat=51.8))
+
+from typing import Any
+
+@dataclass
+class WihoutExpicityTypes:
+    name: Any
+    value: Any = 42
+
+from dataclasses import dataclass
+from math import asin, cos, radians, sin, sqrt
+
+@dataclass
+class Position:
+    name: str
+    lon: float = 0.0
+    lat: float = 0.0
+
+    def distance_to(self, other):
+        r = 6371  # Earth radius in kilometers
+        lam_1, lam_2 = radians(self.lon), radians(other.lon)
+        phi_1, phi_2 = radians(self.lat), radians(other.lat)
+        h = (sin((phi_2 - phi_1) / 2)**2
+             + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
+        return 2 * r * asin(sqrt(h))
+
+
+@dataclass
+class PlayingCard:
+    rank: str
+    suit: str
+
+
+@dataclass
+class Deck:
+    cards: List[PlayingCard]
+
+queen_of_hearts = PlayingCard('Q', 'Hearts')
+ace_of_spades = PlayingCard('A', 'Spades')
+two_cards = Deck([queen_of_hearts, ace_of_spades])
+
+print(two_cards)
