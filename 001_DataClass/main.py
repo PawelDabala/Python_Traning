8b4a19459ff2062129b385c57004ff7936d69027
PawelDabala <pdabala@gmail.com>
2021-09-22 11:47:40 +0200

    frozen class

--- 001_DataClass/main.py
+++ 001_DataClass/main.py
@@ -40,29 +40,29 @@ from typing import List
 # print(Position('Null Island'))
 # print(Position('Greenwich', lat=51.8))
 
-from typing import Any
+# from typing import Any
 
-@dataclass
-class WihoutExpicityTypes:
-    name: Any
-    value: Any = 42
+# @dataclass
+# class WihoutExpicityTypes:
+#     name: Any
+#     value: Any = 42
 
-from dataclasses import dataclass
-from math import asin, cos, radians, sin, sqrt
+# from dataclasses import dataclass
+# from math import asin, cos, radians, sin, sqrt
 
-@dataclass
-class Position:
-    name: str
-    lon: float = 0.0
-    lat: float = 0.0
+# @dataclass
+# class Position:
+#     name: str
+#     lon: float = 0.0
+#     lat: float = 0.0
 
-    def distance_to(self, other):
-        r = 6371  # Earth radius in kilometers
-        lam_1, lam_2 = radians(self.lon), radians(other.lon)
-        phi_1, phi_2 = radians(self.lat), radians(other.lat)
-        h = (sin((phi_2 - phi_1) / 2)**2
-             + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
-        return 2 * r * asin(sqrt(h))
+#     def distance_to(self, other):
+#         r = 6371  # Earth radius in kilometers
+#         lam_1, lam_2 = radians(self.lon), radians(other.lon)
+#         phi_1, phi_2 = radians(self.lat), radians(other.lat)
+#         h = (sin((phi_2 - phi_1) / 2)**2
+#              + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
+#         return 2 * r * asin(sqrt(h))
 
 
 # @dataclass
@@ -108,13 +108,24 @@ class Position:
 # You Need Representation?
 
 #########################################################
-@dataclass
-class PlayingCard:
-    rank: str
-    suit: str
+# @dataclass
+# class PlayingCard:
+#     rank: str
+#     suit: str
 
-    def __str__(self):
-        return f'{self.suit}{self.rank}'
+#     def __str__(self):
+#         return f'{self.suit}{self.rank}'
 
-ace_of_spades = PlayingCard('A', '♠')
-print(ace_of_spades)
+# ace_of_spades = PlayingCard('A', '♠')
+# print(ace_of_spades)
+
+#########################################################
+@dataclass(frozen=True)
+class Position:
+    name: str
+    lon: float = 0.0
+    lat: float = 0.0
+
+pos = Position('Oslo', 10.8, 59.9)
+print(pos.name)
+pos.name = 'Stockholm'
