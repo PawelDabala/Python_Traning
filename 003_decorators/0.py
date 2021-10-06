96c6a8f88c83d7b1d1e2ef46188f097c8f149b47
PawelDabala <pdabala@gmail.com>
2021-10-06 12:21:05 +0200

    send parametrs to decorator

--- 003_decorators/0.py
+++ 003_decorators/0.py
@@ -1,12 +1,18 @@
 def foo():
     return 'bar'
 
+def hello(name):
+    return f'Helo {name}'
+
 #dekorator przyjmuje funkcje
 def uppercase(func):
-    #funkcja która będzie zwrócona
-    def wrapper():
-        return func().upper()
+    #funkcja która będzie zwrócona, wydmuszka
+    # *args i **kwargs są po to aby przekazywać argumenty do fonkcji
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs).upper()
 
     return wrapper
 
 print(uppercase(foo)())
+print(uppercase(hello)('Pawel'))
+
