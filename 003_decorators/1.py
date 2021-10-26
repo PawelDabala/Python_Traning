2e5cf3e240c4474e879c28f3129aed09a40cfd83
PawelDabala <pdabala@gmail.com>
2021-10-26 09:31:24 +0200

    finish 24 modul Pandas

--- 003_decorators/1.py
+++ 003_decorators/1.py
@@ -1,12 +1,3 @@
-#dekorator przyjmuje funkcje
-def uppercase(func):
-    #funkcja która będzie zwrócona, wydmuszka
-    # *args i **kwargs są po to aby przekazywać argumenty do fonkcji
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs).upper()
-
-    return wrapper
-
 def repeat(times):
     def wrapper(func):
         def inner_wrapper(*args, **kwargs):
@@ -24,7 +15,40 @@ def hello(name):
     return f'Helo {name}'
 print(hello('Jaga'))
 
-# print(repeat(4)(hello)("Pawel"))
-   
 
+first_name = 'Jan'
+second_name = "Kowalski "
+print(first_name)
+print(second_name)
 
+print(first_name + second_name)
+
+>>> len("Hellow")
+6
+
+rocket_player1 = "Tom"
+rocket_player2 = "Anna"
+rocket_player3 = "Waldek"
+
+rocket_players = ["Tom", "Anna", "Waldek"]
+
+rocket_players[0]
+rocket_players.append("Jola")
+rocket_players[0] = "Waldek"
+
+#usunięcie gracza o indexie 2
+del rocket_players[2]
+# usunięcie cąłej listy
+del rocket_players
+# usunięcie elementu po wartości
+rocket_players.remove("Anna")
+# usunięcie elementu i zwrócenie
+rocket_players.pop()
+rocket_players.pop(2)
+
+#użytkownik od 1 do 2
+rocket_players[1:3]
+#użytkownik od 1 do końca
+rocket_players[1:]
+#użytkownik do drugiego włącznie
+rocket_players[:3]
