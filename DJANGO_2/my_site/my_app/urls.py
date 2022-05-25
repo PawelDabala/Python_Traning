13f0d1a5555e5527e46bb4db189e0170e95bad0a
PawelDabala <pdabala@gmail.com>
2022-05-25 13:43:54 +0200

    09.06 Dynamic Views

--- DJANGO_2/my_site/my_app/urls.py
+++ DJANGO_2/my_site/my_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('sports', views.news_view)
+# ]
+
+#przkanie argumentu do dynamicznego url:
 urlpatterns = [
-    path('simple_view', views.simple_view)
+    path('<str:topic>/', views.news_view),
+    path('<int:num1>/<int:num2>', views.add_view),
 ]
+
--- DJANGO_2/my_site/my_app/views.py
+++ DJANGO_2/my_site/my_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
-# Create your views here.
-def simple_view(request):
-    return HttpResponse('SIMPLE VIEW')
+articles = {
+    'sports': 'Sports Page',
+    'finance': 'Finance Page',
+    'politics': 'Politics Page'
+}
 
+# Create your views here.
+# def sport_view(request):
+#     return HttpResponse(articles['sports'])
+
+# def finance_view(request):
+#     return HttpResponse(articles['finance'])
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    #domian.com/my_app/num1/num2  --> num1+num2
+    add_result = num1 + num2
+    result = f'{num1}+{num2} = {add_result}'
+    return HttpResponse(str(result))
